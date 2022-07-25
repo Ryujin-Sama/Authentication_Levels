4648d1bcaa899077f0fcbb3098fd823d6c25604b
Ryujin Sama <97237040+Ryujin-Sama@users.noreply.github.com>
2022-07-25 10:42:19 +0530

    Second Commit
    - Using lvl 4 authentication with md5

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-
+const md5 = require("md5")
 // For Lvl 2 Authentication using Mongoose encryption
 
 const encrypt = require("mongoose-encryption");
@@ -34,7 +34,7 @@ const userSchema = new mongoose.Schema({
 
 // Mongoose encryption plugin
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
+
 
 //Creating ModelDB
 
@@ -61,7 +61,7 @@ app.get("/register", function(req,res){
 app.post("/register", function(req,res){
   const newUser = new User({
     email : req.body.username,
-    password : req.body.password
+    password : md5(req.body.password)
   });
   newUser.save(function(err){
     if(!err){
@@ -77,7 +77,7 @@ app.post("/register", function(req,res){
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, function(err, foundUser){
     if(!err){
