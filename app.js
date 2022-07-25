8ae18e084446cc017fce2363ac27931a510ae5c4
Ryujin Sama <97237040+Ryujin-Sama@users.noreply.github.com>
2022-07-25 23:53:58 +0530

    Fourth Commit - Lvl - 6
    Lvl -6 Authentication using Passport.js  & Express - session

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 // For lvl- 3 Authentication using environment variables
 // For lvl -4 Authentication using Hashing with md5
 // Lvl-5 Authentication using Hashing and salting with bcrypt
+// Lvl-6 Authentication using Passport.js & Express-Session
 
 require('dotenv').config();
 const express = require("express");
@@ -10,8 +11,11 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const md5 = require("md5");
-const bcrypt = require("bcrypt");
-const saltRounds = 10  // On increasing the no of rounds the cpu usage is higher inorder to generate the hashes
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+//const bcrypt = require("bcrypt");
+//const saltRounds = 10  // On increasing the no of rounds the cpu usage is higher inorder to generate the hashes
 
 // For Lvl 2 Authentication using Mongoose encryption
 
@@ -26,6 +30,19 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+// Using Express-Session package with some default configs
+
+app.use(session({
+  secret : "Our Little secrets.",
+  resave : false,
+  saveUninitialized: false
+}));
+
+// Using Passport Package
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Connecting mongoose
 
 mongoose.connect("mongodb://localhost:27017/userDB");
@@ -37,14 +54,19 @@ const userSchema = new mongoose.Schema({
   password : String
 });
 
-// Mongoose encryption plugin
+// Passport plugin
 
-
+userSchema.plugin(passportLocalMongoose);
 
 //Creating ModelDB
 
 const User = new mongoose.model("User", userSchema);
 
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // Get files
 
@@ -60,12 +82,34 @@ app.get("/register", function(req,res){
   res.render("register");
 });
 
+// Adding SECRET page to work only when passwords are matching
+
+app.get("/secrets", function(req,res){
+  if(req.isAuthenticated()){
+    res.render("secrets");
+  }
+  else{
+    res.render("login");
+  }
+});
+
+// Redirecting user to login Page once they Logout
+
+app.get("/logout", function(req,res, next){
+  req.logout(function(err){
+    if(err){
+      return next(err);
+    }
+    res.redirect("/");
+  });
+
+});
 
 // Creating Register file
 
 app.post("/register", function(req,res){
 
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+  /*bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
     const newUser = new User({
       email : req.body.username,
       password : hash  //md5(req.body.password)  // using md5 for Hashing
@@ -78,13 +122,29 @@ app.post("/register", function(req,res){
         console.log(err);
       }
     });
+  });*/
+
+
+// New Register Code Using Passport
+
+  User.register({username : req.body.username}, req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      res.redirect("/register");
+    }
+    else{
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+      });
+    }
   });
+
 });
 
 //Creating Login file
 
 app.post("/login", function(req, res){
-  const username = req.body.username;
+  /* const username = req.body.username;
   const password =  req.body.password; //md5(req.body.password);  //using md5 for Hashing
 
   User.findOne({email: username}, function(err, foundUser){
@@ -101,6 +161,27 @@ app.post("/login", function(req, res){
       console.log(err);
     }
   });
+  */
+
+// New Login Code using Passport
+
+
+  const user = new User({
+    username : req.body.username,
+    password : req.body.password
+  });
+
+  req.login(user, function(err){
+    if(err){
+      console.log(err);
+    }
+    else{
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+      });
+    }
+  });
+
 });
 
 // Server starting at port 3000
