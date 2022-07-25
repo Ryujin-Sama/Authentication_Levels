37a3e59fc24bffddc97b5bcac0e2748410ec81c6
Ryujin Sama <97237040+Ryujin-Sama@users.noreply.github.com>
2022-07-25 23:01:38 +0530

    Third Commit - Lvl 5
    Lvl 5 Encryption using hashing and salting with bcrypt

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 
 // Requiring Module
 // For lvl- 3 Authentication using environment variables
+// For lvl -4 Authentication using Hashing with md5
+// Lvl-5 Authentication using Hashing and salting with bcrypt
 
 require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5")
+const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10  // On increasing the no of rounds the cpu usage is higher inorder to generate the hashes
+
 // For Lvl 2 Authentication using Mongoose encryption
 
-const encrypt = require("mongoose-encryption");
+//const encrypt = require("mongoose-encryption");
 
 
 const app = express();
@@ -59,32 +64,37 @@ app.get("/register", function(req,res){
 // Creating Register file
 
 app.post("/register", function(req,res){
-  const newUser = new User({
-    email : req.body.username,
-    password : md5(req.body.password)
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email : req.body.username,
+      password : hash  //md5(req.body.password)  // using md5 for Hashing
+    });
+    newUser.save(function(err){
+      if(!err){
+        res.render("secrets");
+      }
+      else{
+        console.log(err);
+      }
+    });
   });
-  newUser.save(function(err){
-    if(!err){
-      res.render("secrets");
-    }
-    else{
-      console.log(err);
-    }
-  })
 });
 
 //Creating Login file
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password =  req.body.password; //md5(req.body.password);  //using md5 for Hashing
 
   User.findOne({email: username}, function(err, foundUser){
     if(!err){
       if(foundUser){
-        if(foundUser.password === password){
-          res.render("secrets");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+            if(result == true){
+              res.render("secrets");
+            }
+        });
       }
     }
     else{
